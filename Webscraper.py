7e727e4be83f67cd5798138ed5769cf1274d5e30
Jaden Millington <57372238+blaze500@users.noreply.github.com>
2022-03-09 02:39:30 -0500

    Add files via upload

--- Webscraper.py
+++ Webscraper.py
@@ -6,15 +6,17 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 import datetime
 import random
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import re
 from pandas import DataFrame
 from urllib.parse import urlparse
 import datetime
 import re
 
-#PATH = 'C:\Program Files (x86)\chromedriver.exe'
-#driver= webdriver.Chrome(PATH)
 
 class JobFinder:
 
@@ -217,6 +219,7 @@ class GoogleCareerJobs:
         self.education = education
         self.experience = experience
         self.writeTo = writeTo
+        self.seleniumDriver = webdriver.Chrome("chromedriver.exe")
 
     def googleJobsLinkMaker(self):
 
@@ -255,6 +258,145 @@ class GoogleCareerJobs:
 
     def linkToHTML(self, link):
         #Gets URL, add .content to turn it into something readable
+        self.seleniumDriver.get(link.strip())
+        time.sleep(1)
+        try:
+            WebDriverWait(self.seleniumDriver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "li"))
+            )
+        except:
+            self.seleniumDriver.quit()
+            self.seleniumDriver.close()
+            return None
+
+        return self.seleniumDriver.find_elements(By.TAG_NAME, "a")
+
+
+    def findLinks(self):
+        link = self.googleJobsLink
+        page = 1
+        while link != "":
+            a_tags = self.linkToHTML(link)
+            print("current link: " + link)
+            #print(urlHTML)
+            if a_tags is not None:
+                link = self.linkAlgorithm(a_tags, link, page)
+                page += 1
+                print("nextLink= " + link)
+            else:
+                print("Did not find what you are looking for!")
+                break;
+        self.seleniumDriver.quit()
+        print("Task Ended Sucessfully")
+
+    def isProperLink(self, url):
+        # Checks if the href is actually a word
+        if url is not None:
+            # Checks if the href is actually there (at least one letter)
+            if len(url) > 0:
+                # Checks to make sure it is not a downloadable file type (i.e, the link is not sending you to a download)
+                if "pdf" not in url and "zIp" not in url and "zip" not in url \
+                        and "win" not in url and "mp3" not in url and "mp4" not in \
+                        url and "jpg" not in url and "JPEG" not in url and \
+                        "jpeg" not in url and "docx" not in url and "pptx" not in \
+                        url and "png" not in url:
+                    return True
+
+        return False
+
+    def linkConditions(self, url, link, page):
+        if "/jobs/results/" in url and "/jobs/results/?" not in url:
+            return True
+        elif "/jobs/results/?" in url and "page="+str(page+1) in url:
+            return True
+        return False
+
+    def linkAlgorithm(self, a_tags, link, page):
+        nextLink=""
+        for a_tag in a_tags:
+            url=a_tag.get_attribute("href")
+            #print(url)
+            #
+            if self.isProperLink(url):
+                #
+                if self.linkConditions(url, link, page):
+                    # checks to see if that is the link to the next page
+                    if "page="+str(page+1) in url:
+                        print(url)
+                        nextLink=url
+                    # if its not, write it to a csv
+                    else:
+                        #print(url)
+                        #Because I am not looking for new links, I will
+                        if url not in open(self.writeTo + '.csv', encoding="utf-8").read():
+                            self.writeToCSV(url)
+        return nextLink
+
+    def writeToCSV(self, finalURL):
+        csv = open(self.writeTo + '.csv', 'a', encoding="utf-8")
+        csv.write(finalURL + "\n")
+        csv.close()
+
+JobFinder()
+
+
+
+
+
+"""
+
+class GoogleCareerJobs:
+
+    def __init__(self, type, field, inPerson, fullTime, salary, location, education, experience, writeTo):
+        self.type = type
+        self.field = field
+        self.inPerson = inPerson
+        self.fullTime = fullTime
+        self.salary = salary
+        self.location = location
+        self.education = education
+        self.experience = experience
+        self.writeTo = writeTo
+        self.selenium = webdriver.Chrome("chromedriver.exe")
+
+    def googleJobsLinkMaker(self):
+
+        self.googleJobsLink = "https://careers.google.com/jobs/results/"
+
+        if self.education == "associate":
+            self.googleJobsLink += "&degree=ASSOCIATE"
+        elif self.education == "bachelor":
+            self.googleJobsLink +="&degree=BACHELOR"
+        elif self.education == "masters":
+            self.googleJobsLink +="&degree=MASTERS"
+
+        if self.fullTime == "full time":
+            self.googleJobsLink +="&employment_type=FULL_TIME"
+        elif self.fullTime == "part time":
+            self.googleJobsLink +="&employment_type=PART_TIME"
+        elif self.fullTime == "both":
+            self.googleJobsLink +="&employment_type=FULL_TIME&employment_type=PART_TIME"
+
+        if self.type == "internship":
+            self.googleJobsLink += "&employment_type=INTERN"
+
+        if self.inPerson != "in person":
+            self.googleJobsLink +="&has_remote=true"
+
+        if len(self.location) > 0:
+            self.googleJobsLink +="&location=" + self.location.replace(" ", "%20")
+
+        if self.field == "":
+            self.googleJobsLink +="&q=" + self.field.replace(" ", "%20")
+
+        if len(self.googleJobsLink) > 40:
+            self.googleJobsLink = self.googleJobsLink.replace("https://careers.google.com/jobs/results/&", "https://careers.google.com/jobs/results/?")
+
+        print(self.googleJobsLink)
+
+    def linkToHTML(self, link):
+        #Gets URL, add .content to turn it into something readable
+        print(link.strip())
         url = requests.get(link.strip())
         print(url.content)
         #Turns Page into HTML
@@ -315,4 +457,134 @@ class GoogleCareerJobs:
         csv.write(finalURL + "\n")
         csv.close()
 
-JobFinder()
+"""
+
+"""
+class GoogleCareerJobs:
+
+    def __init__(self, type, field, inPerson, fullTime, salary, location, education, experience, writeTo):
+        self.type = type
+        self.field = field
+        self.inPerson = inPerson
+        self.fullTime = fullTime
+        self.salary = salary
+        self.location = location
+        self.education = education
+        self.experience = experience
+        self.writeTo = writeTo
+        self.seleniumDriver = webdriver.Chrome("chromedriver.exe")
+
+    def googleJobsLinkMaker(self):
+
+        self.googleJobsLink = "https://careers.google.com/jobs/results/"
+
+        if self.education == "associate":
+            self.googleJobsLink += "&degree=ASSOCIATE"
+        elif self.education == "bachelor":
+            self.googleJobsLink +="&degree=BACHELOR"
+        elif self.education == "masters":
+            self.googleJobsLink +="&degree=MASTERS"
+
+        if self.fullTime == "full time":
+            self.googleJobsLink +="&employment_type=FULL_TIME"
+        elif self.fullTime == "part time":
+            self.googleJobsLink +="&employment_type=PART_TIME"
+        elif self.fullTime == "both":
+            self.googleJobsLink +="&employment_type=FULL_TIME&employment_type=PART_TIME"
+
+        if self.type == "internship":
+            self.googleJobsLink += "&employment_type=INTERN"
+
+        if self.inPerson != "in person":
+            self.googleJobsLink +="&has_remote=true"
+
+        if len(self.location) > 0:
+            self.googleJobsLink +="&location=" + self.location.replace(" ", "%20")
+
+        if self.field == "":
+            self.googleJobsLink +="&q=" + self.field.replace(" ", "%20")
+
+        if len(self.googleJobsLink) > 40:
+            self.googleJobsLink = self.googleJobsLink.replace("https://careers.google.com/jobs/results/&", "https://careers.google.com/jobs/results/?")
+
+        print(self.googleJobsLink)
+
+    def linkToHTML(self, link):
+        #Gets URL, add .content to turn it into something readable
+        self.seleniumDriver.get(link.strip())
+        try:
+            a_tags = WebDriverWait(self.seleniumDriver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "li"))
+            )
+        except:
+            self.seleniumDriver.quit()
+            self.seleniumDriver.close()
+            return None
+
+        return self.seleniumDriver.find_elements(By.TAG_NAME, "a")
+
+
+    def findLinks(self):
+        link = self.googleJobsLink
+        page = 1
+        while link != "":
+            a_tags = self.linkToHTML(link)
+            #print(urlHTML)
+            if a_tags is not None:
+                link = self.linkAlgorithm(a_tags, link, page)
+                page += 1
+                print("nextLink= " + link)
+            else:
+                print("Did not find what you are looking for!")
+                break;
+
+        self.seleniumDriver.close()
+
+    def isProperLink(self, url):
+        # Checks if the href is actually a word
+        if url is not None:
+            # Checks if the href is actually there (at least one letter)
+            if len(url) > 0:
+                # Checks to make sure it is not a downloadable file type (i.e, the link is not sending you to a download)
+                if "pdf" not in url and "zIp" not in url and "zip" not in url \
+                        and "win" not in url and "mp3" not in url and "mp4" not in \
+                        url and "jpg" not in url and "JPEG" not in url and \
+                        "jpeg" not in url and "docx" not in url and "pptx" not in \
+                        url and "png" not in url:
+                    return True
+
+        return False
+
+    def linkConditions(self, url, link, page):
+        if "/jobs/results/" in url and "/jobs/results/?" not in url:
+            #print("true")
+            return True
+        elif link in url and "page="+str(page+1) in url:
+            return True
+        return False
+
+    def linkAlgorithm(self, a_tags, link, page):
+        nextLink=""
+        for a_tag in a_tags:
+            url=a_tag.get_attribute("href")
+            #print(url)
+            #
+            if self.isProperLink(url):
+                #
+                if self.linkConditions(url, link, page):
+                    # checks to see if that is the link to the next page
+                    if "page="+str(page+1) in url:
+                        nextLink=url
+                    # if its not, write it to a csv
+                    else:
+                        print(url)
+                        #Because I am not looking for new links, I will
+                        self.writeToCSV(url)
+        return nextLink
+
+    def writeToCSV(self, finalURL):
+        csv = open(self.writeTo + '.csv', 'a', encoding="utf-8")
+        csv.write(finalURL + "\n")
+        csv.close()
+
+"""
